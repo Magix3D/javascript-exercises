941eb30b75af247dd2009c6005c91cf060069846
Magix3D <80196203+Magix3D@users.noreply.github.com>
2026-06-20 01:49:09 +0200

    Solution for 07_reverseString

--- foundations/07_reverseString/reverseString.js
+++ foundations/07_reverseString/reverseString.js
@@ -1,5 +1,9 @@
-const reverseString = function() {
-
+const reverseString = function(input) {
+    let output = "";
+    for(let i = input.length; i > 0; i--) {
+        output += input.charAt(i - 1)
+    };
+    return output
 };
 
 // Do not edit below this line
