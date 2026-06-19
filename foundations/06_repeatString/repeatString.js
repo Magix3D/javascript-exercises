adc6589c67c471acd6a626a24649dce442d7cb25
Magix3D <80196203+Magix3D@users.noreply.github.com>
2026-06-20 01:31:20 +0200

    Solution for 06_repeatString

--- foundations/06_repeatString/repeatString.js
+++ foundations/06_repeatString/repeatString.js
@@ -1,5 +1,13 @@
-const repeatString = function() {
-
+const repeatString = function(text, iterations) {
+    if(Number(iterations) < 0) {
+        return "ERROR"
+    } else {
+        let output = "";
+        for(; iterations > 0; iterations--) {
+             output += text
+        };
+        return output
+    }
 };
 
 // Do not edit below this line
