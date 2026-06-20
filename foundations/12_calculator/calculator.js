4df8b4fe456d16fe2ad9e0022cf9a28cd02e01fc
Magix3D <80196203+Magix3D@users.noreply.github.com>
2026-06-20 17:18:28 +0200

    Solution for 12_calculator

--- foundations/12_calculator/calculator.js
+++ foundations/12_calculator/calculator.js
@@ -1,26 +1,37 @@
-const add = function() {
-	
+const add = function(num1, num2) {
+	return num1 + num2
 };
 
-const subtract = function() {
-	
+const subtract = function(num1, num2) {
+	return num1 - num2
 };
 
-const sum = function() {
-	
+const sum = function(numbers) {
+  let sum = 0
+	numbers.forEach(
+    item =>
+    sum += item
+  );
+  return sum
 };
 
-const multiply = function() {
-
+const multiply = function(numbers) {
+  return numbers.reduce((total, current) => total * current)
 };
 
-const power = function() {
-	
+const power = function(num1, num2) {
+	return num1 ** num2
 };
 
-const factorial = function() {
-	
+const factorial = function(num) {
+  let sum = 1
+	for(let i = num; i > 0; i--) {
+    sum *= i
+  }
+  return sum
 };
+
+console.log(factorial(4))
 
 // Do not edit below this line
 module.exports = {
