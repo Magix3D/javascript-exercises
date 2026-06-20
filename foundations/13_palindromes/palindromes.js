b74d7b133886d894b996d7ecd00c14b14a272293
Magix3D <80196203+Magix3D@users.noreply.github.com>
2026-06-20 17:40:51 +0200

    Solution for 13_palindromes

--- foundations/13_palindromes/palindromes.js
+++ foundations/13_palindromes/palindromes.js
@@ -1,6 +1,24 @@
-const palindromes = function () {
-
+const palindromes = function (string) {
+  return (
+    string
+      .toLowerCase()
+      .replaceAll(",", "")
+      .replaceAll(" ", "")
+      .replaceAll(".", "")
+      .replaceAll("!", "") ===
+    string
+      .split("")
+      .reverse()
+      .join("")
+      .toLowerCase()
+      .replaceAll(",", "")
+      .replaceAll(" ", "")
+      .replaceAll(".", "")
+      .replaceAll("!", "")
+  );
 };
+
+console.log(palindromes("Racecar"));
 
 // Do not edit below this line
 module.exports = palindromes;
