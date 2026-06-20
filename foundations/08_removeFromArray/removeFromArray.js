cdb83b09abd734b947c3f3c64adbc2f59aa28956
Magix3D <80196203+Magix3D@users.noreply.github.com>
2026-06-20 02:18:14 +0200

    Solution for 08_removeFromArray

--- foundations/08_removeFromArray/removeFromArray.js
+++ foundations/08_removeFromArray/removeFromArray.js
@@ -1,5 +1,8 @@
-const removeFromArray = function() {
+const removeFromArray = function(array, ...remove) {
+    return array.filter(pos => !remove.includes(pos))
 };
+
+console.log(removeFromArray([1, 2, 3, 4], 3))
 
 // Do not edit below this line
 module.exports = removeFromArray;
