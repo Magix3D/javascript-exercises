e587af75aad0b54d4c8f9d7be9717b70bd749319
Magix3D <80196203+Magix3D@users.noreply.github.com>
2026-06-20 18:33:31 +0200

    Solution for 16_findTheOldest

--- foundations/16_findTheOldest/findTheOldest.js
+++ foundations/16_findTheOldest/findTheOldest.js
@@ -1,6 +1,39 @@
-const findTheOldest = function() {
+const findTheOldest = function (people) {
+  let oldest = 0;
+  let lastAge = 0;
 
+  for (let i = 0; i < people.length; i++) {
+    const deathYear = people[i].yearOfDeath ?? new Date().getFullYear();
+    const age = deathYear - people[i].yearOfBirth;
+
+    if (age > lastAge) {
+      oldest = i;
+      lastAge = age;
+    }
+  }
+
+  return people[oldest];
 };
+
+
+console.log(
+  findTheOldest([
+      {
+        name: "Carly",
+        yearOfBirth: 2018,
+      },
+      {
+        name: "Ray",
+        yearOfBirth: 1962,
+        yearOfDeath: 2011,
+      },
+      {
+        name: "Jane",
+        yearOfBirth: 1912,
+        yearOfDeath: 1941,
+      },
+    ]),
+);
 
 // Do not edit below this line
 module.exports = findTheOldest;
